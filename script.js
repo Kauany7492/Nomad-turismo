7c804ecb1371fb7c3af13a3cc69e23edb9e418e0
Kauany7492 <140413644+Kauany7492@users.noreply.github.com>
2024-07-17 14:20:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
 
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.remove('active');
-        if (i === index) {
-            slide.classList.add('active');
-        }
-    });
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-}
-
-setInterval(nextSlide, 3000);
-
-function toggleMenu() {
-    const menu = document.getElementById('menu').querySelector('ul');
-    menu.classList.toggle('active');
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentSlide);
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
